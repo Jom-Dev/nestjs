7842afbf9388f47a2a8c6efd91d4c0d2166a2204
Jom-Dev <espino.jomarie@gmail.com>
2024-09-03 08:36:01 +0800

    database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NinjasModule } from './ninjas/ninjas.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NinjasModule, UsersModule],
+  imports: [ConfigModule.forRoot(), NinjasModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
