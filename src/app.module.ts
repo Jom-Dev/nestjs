95ee7eb565333de6388790e17a614bbe3df03228
Jom-Dev <espino.jomarie@gmail.com>
2024-09-05 19:39:33 +0800

    setting up db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { NinjasModule } from './ninjas/ninjas.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TodosModule } from './todos/todos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'ormconfig'; 
+
 
 @Module({
-  imports: [ConfigModule.forRoot(), NinjasModule, UsersModule, TodosModule],
+  imports: [TypeOrmModule.forRoot(config), 
+  ConfigModule.forRoot(), NinjasModule, UsersModule, TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
