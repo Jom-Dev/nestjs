2b8cfa57e952825441bf5d70a34d7303107ad686
Jom-Dev <espino.jomarie@gmail.com>
2024-09-13 06:59:26 +0800

    updated app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,10 +10,20 @@ import { Meeting } from './employees/entities/meeting.entity';
 export class AppService {
   constructor (
     @InjectRepository(Employee) private employeeRepo: Repository<Employee>,
-    @InjectRepository(ContactInfo) private contactInfo: Repository<ContactInfo>,
+    @InjectRepository(ContactInfo) private contactInfoRepo: Repository<ContactInfo>,
     @InjectRepository(Meeting) private meetingRepo: Repository<Meeting>,
     @InjectRepository(Task) private taskRepo: Repository<Task>,
    ) {}
+
+   async seed() {
+    const ceo = this.employeeRepo.create({ name: 'John' });
+    await this.employeeRepo.save(ceo);
+
+    const ceoContactInfo = this.contactInfoRepo.create({ email: 'test@test.com' });
+    
+    ceoContactInfo.employee = ceo;
+    await this.contactInfoRepo.save(ceoContactInfo);
+   }
 
   getHello(): string {
     return 'Hello World!';
